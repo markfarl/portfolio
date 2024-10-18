c6cdc0f31143a527786564988fa3a56910a9a4a6
markfarl <markfarl@gmail.com>
2024-10-18 14:45:11 +0100

    updates

--- src/view/NodejsSkill.tsx
+++ src/view/NodejsSkill.tsx
@@ -1,13 +1,24 @@
 export default function NodejsSkill() {
     return (
         <div className="p-5">
-            <p className="mb-5">Lorem ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum</p>
-            <p className="mb-5">Some recent projects include:</p>
+            <p className="mb-5">I have developed backend applications with Node.js for use as API or CLI scripts to parse and process data.</p>
+            <p className="mb-5">Some example use cases would include:</p>
             <ul className="ml-10 list-disc">
-                <li>Project N</li>
-                <li>Project N</li>
-                <li>Project N</li>
-                <li>Project N</li>
+                <li>Testing endpoints to validate payload data from E2E tests
+                    <ul className="mb-5 ml-10 list-disc italic">
+                        <li>
+                            Docker, AWS, Jenkins, Github actions
+                        </li>
+                    </ul>
+                </li>
+                <li>Tasks parsing data from external files into JSON and uploading to cloud
+                    <ul className="mb-5 ml-10 list-disc italic">
+                        <li>
+                            Google Cloud Platform, Firestore, Google Tag Manager
+
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </div>
     )
--- src/view/TestingSkill.tsx
+++ src/view/TestingSkill.tsx
@@ -1,7 +1,7 @@
 export default function TestingSkill() {
     return (
         <div className="p-5">
-            <p className="mb-5">Lorem ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum</p>
+            <p className="mb-5">Coming soon</p>
             <p className="mb-5">Some recent projects include:</p>
             <ul className="ml-10 list-disc">
                 <li>Project N</li>
