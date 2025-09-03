33362b40ce6cb6003d21428463074a339abfff9f
markfarl <markfarl@gmail.com>
2025-09-03 13:53:26 +0100

    updates

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -39,21 +39,23 @@ export default function Skills() {
       <main className={`transform transition duration-200 border-t-2 pb-10 ${colorSchemeClass.borderLine}`}>
 
         <div className="container md:mx-auto max-w-screen-xl p-8 xl:p-0">
-          <section id="topRef" className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:grid-rows-2 gap-3 mt-10">
-            <div className={`${isAnimatingIn ? "animate-in fade-in slide-in-from-top duration-500" : "transform transition duration-200"} border-2 row-span-3 sm:col-span-3 ${colorSchemeClass.borderLine} main-tiles ${colorSchemeClass.tilesBk}`}>
-              {getImg?.img && (
-                <div className="p-3 text-center">
-                  <img src={getImg.img} alt="Selected tile" className="mx-auto max-h-24 object-contain" />
-                </div>
-              )}
-              <Outlet />
-            </div>
+          <div className="flex justify-end">
 
             <Link to={`/`}>
-              <div className={`transform transition duration-200 hover:scale-105 border-2 w-[40px] h-[40px] flex items-center justify-center ${colorSchemeClass.borderLine} main-tiles ${colorSchemeClass.tilesBk}`}>
+              <div id="topRef" className={`transform transition duration-200 hover:scale-105 border-2 border-b-0 mt-10 w-[40px] h-[40px] flex items-center justify-center ${colorSchemeClass.borderLine} main-tiles ${colorSchemeClass.tilesBk}`}>
                 <XMarkIcon className="w-6 h-6" aria-hidden="true" />
               </div>
             </Link>
+          </div>
+          <section className={`border-2 ${colorSchemeClass.borderLine} ${colorSchemeClass.tilesBk} ${isAnimatingIn ? "animate-in fade-in slide-in-from-top duration-500" : "transform transition duration-200"} grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:grid-rows-2 gap-3`}>
+            <div className={` row-span-3 sm:col-span-3  main-tiles `}>
+
+              <Outlet />
+            </div>
+
+            {getImg?.img && (
+              <img src={getImg.img} alt="Selected tile" className="mx-auto max-h-24 object-contain mt-10" />
+            )}
 
           </section>
 
